fed1d9eae70a0a557b6bf47b04327e34eb342aae
Alejandro CR <contacto@alejandrocr.co>
2026-03-14 19:24:16 -0500

    Add health check endpoint and update router inclusion for tokens and users

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@
 """Bootstrap the FastAPI server application."""
 from __future__ import annotations
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from server.auth.tokens.routes import router as tokens_router
 from server.auth.users.routes import router as users_router
@@ -10,5 +10,9 @@ from server.shared.env import env
 
 server = FastAPI(title=env.get('APP_NAME'))
 
-server.include_router(tokens_router)
-server.include_router(users_router)
+@server.get('/health', status_code=status.HTTP_204_NO_CONTENT)
+def health_check():
+    """Endpoint to check the health of the server"""
+
+server.include_router(tokens_router, prefix='/api')
+server.include_router(users_router, prefix='/api')
