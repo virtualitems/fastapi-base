fb1e6afde23b67599fb0e57a1b52ab76eab43d99
Alejandro CR <contacto@alejandrocr.co>
2026-03-18 02:26:20 -0500

    feat: add data transfer objects for authentication module

---
+++ server/auth/authentication/dtos.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+"""Authentication module data models"""
+from __future__ import annotations
+
+from pydantic import BaseModel, EmailStr, Field
+
+class LoginDTO(BaseModel):
+    """Data transfer object for login"""
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=100)
+
+class TokenDTO(BaseModel):
+    """Data transfer object for token response"""
+    access_token: str
+    token_type: str
+
+class TokenDataDTO(BaseModel):
+    """Data transfer object for token data"""
+    sub: str | None = None
